9069c45bc944dbbd8b781374e1c49fa1ee4229a2
joaocsf <noobcsf@hotmail.com>
2018-11-04 11:15:47 +0000

    Updated Consult

--- Data/rScripts/consult.R
+++ Data/rScripts/consult.R
@@ -71,15 +71,12 @@ group_by(account_id) %>%
 
 disp_owners <- filter(disp,type=="OWNER")
 clients_accounts <- subset(left_join(disp_owners, clients, by='client_id'), select=-district_id)
-account_district <- subset(left_join(account, district, by=c('district_id'='code')), select=c(account_id, district_id, name, region))
+#account_district <- subset(left_join(account, district, by=c('district_id'='code')), select=c(account_id, district_id, name, region))
+account_district <- left_join(account, district, by=c('district_id'='code'))
 account_district_client <- left_join(clients_accounts, account_district, 'account_id')
 trans_client_district <- left_join(trans_calculations, account_district_client, 'account_id')
 
 
-
-colnames(loan_order_by_account_not_approved)[2] <- "amount_recused"
-
-aproved_not_aproved <- left_join(loan_order_by_account_not_approved, loan_order_by_account_approved, 'account_id')
 
 
 trans_client_district_loan <- left_join(loan, trans_client_district, 'account_id')
@@ -127,7 +124,25 @@ group_by_age_range <- trans_client_district %>%
     avg_balance=mean(avg_balance)
   )
 
+#Approved Loans
+#ggplot(trans_client_district_loan[trans_client_district_loan$status>0,], mapping=aes(x=age_range)) + geom_histogram(binwidth = 300, stat="count")
+# + theme_bw() + coord_cartesian() + scale_color_gradient()
+#Paid vs noy paid Loans
+#ggplot(trans_client_district_loan, mapping=aes(x=status, group=status)) + geom_bar(stat="count") + xlab("Status") + ylab("Number of Loans") + ggtitle("Number of Paid vs Not Paid Loans")
+
 #ggplot(data = group_by_age_range) + geom_bar(mapping=aes(x=age_range, y=avg_amount), stat="identity") + xlab('Decades') + ylab('Averaged Credited Amount (M.U)') + ggtitle('Averaged Credited Amount Grouped By Decades') + theme_bw() + coord_cartesian() + scale_color_gradient()
 #ggplot(data = group_by_age_range) + geom_bar(mapping=aes(x=age_range, y=avg_balance), stat="identity") + xlab('Decades') + ylab('Average Balance (M.U)') + ggtitle('Average Balance Grouped By Decades') + theme_bw() + coord_cartesian() + scale_color_gradient()
 #ggplot(data = trans_client_district) + geom_point(mapping=aes(x=avg_credit, y=avg_withdrawal), stat="identity") + theme_bw() + coord_cartesian() + scale_color_gradient() + geom_abline(intercept=0, slope=1, linetype="dashed", color="red", size=1.25) + xlab("Average Credit") + ylab("Average Withdrawal") + ggtitle("Comparison of the Averages Between Credit and Withdrawal (per account).") 
 print(trans_calculations)
+#[1] "loan_id"               "account_id"            "date.x"                "amount"               
+#[5] "duration"              "payments"              "status"                "max_credit"           
+#[9] "avg_credit"            "min_credit"            "max_withdrawal"        "avg_withdrawal"       
+#[13] "min_withdrawal"        "avg_amount"            "avg_balance"           "max_balance"          
+#[17] "min_balance"           "disp_id"               "client_id"             "type"                 
+#[21] "birthday"              "gender"                "age_range"             "district_id"          
+#[25] "frequency"             "date.y"                "name"                  "region"               
+#[29] "hab"                   "m_hab.499"             "m_hab500.1999"         "m_hab2000.9999"       
+#[33] "m_hab.10000"           "cities"                "urban_hab"             "salary"               
+#[37] "unemployment95"        "unemployment96"        "enterpreneurs_per_hab" "crimes95"             
+#[41] "crimes96" 
+
